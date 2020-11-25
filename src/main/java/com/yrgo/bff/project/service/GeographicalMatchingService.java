70f0da306c5d0351c8ca5bc82c044ddd2131d6cb
Johan Nyberg <johan.e.nyberg@icloud.com>
2020-11-25 12:36:49 +0100

    simplified

--- src/main/java/com/yrgo/bff/project/service/GeographicalMatchingService.java
+++ src/main/java/com/yrgo/bff/project/service/GeographicalMatchingService.java
@@ -3,16 +3,9 @@ package com.yrgo.bff.project.service;
 import com.yrgo.bff.project.domain.GpsCoordinates;
 
 public class GeographicalMatchingService {
-    //reach - range in kilometers
-
     /**
      *
      * GeographicalMatchingService
-     *
-     * Definitions
-     * Latitude: 1 deg = 110.574 km
-     * Longitude: 1 deg = 111.320*cos(latitude) km
-     *
      *
      * @author Johan Nyberg
     * */
@@ -25,59 +18,6 @@ public class GeographicalMatchingService {
                 isWithinInterval(playerB.getLatitude(),playerA.getLatitude()-range,playerA.getLatitude()+range) &&
                 isWithinInterval(playerB.getLongitude(),playerA.getLongitude()-range,playerB.getLongitude()+range));
 
-    }
-
-    public boolean matchApprox(GpsCoordinates coordinatesPlayerA, double rangePlayerA,
-                         GpsCoordinates coordinatesPlayerB, double rangePlayerB){
-
-        double  aLat, aMinLat,aMaxLat, aLong, aMinLong, aMaxLong,
-                bLat, bMinLat, bMaxLat, bLong, bMinLong, bMaxLong;
-
-        //convert from km to degrees
-
-        aLat = coordinatesPlayerA.getLatitude();
-        aMinLat = aLat-kilometersToDegreesLatitude(rangePlayerA);
-        aMaxLat = aLat+kilometersToDegreesLatitude(rangePlayerA);
-        aLong = coordinatesPlayerA.getLongitude();
-        aMinLong = aLong-kilometersToDegreesLongitude(rangePlayerA);
-        aMaxLong = aLong+kilometersToDegreesLongitude(rangePlayerA);
-
-        bLat = coordinatesPlayerB.getLatitude();
-        bMinLat = bLat-kilometersToDegreesLatitude(rangePlayerB);
-        bMaxLat = bLat+kilometersToDegreesLatitude(rangePlayerB);
-        bLong = coordinatesPlayerB.getLongitude();
-        bMinLong = bLong-kilometersToDegreesLongitude(rangePlayerB);
-        bMaxLong = bLong+kilometersToDegreesLongitude(rangePlayerB);
-
-        if (isWithinInterval(aLat,bMinLat,bMaxLat) &&
-                isWithinInterval(aLong,bMinLong,bMaxLong) &&
-                isWithinInterval(bLat,aMinLat,aMaxLat) &&
-                isWithinInterval(bLong,aMinLong,aMaxLong))
-        {
-            return true;
-        } else {
-            return false;
-        }
-
-
-        //TODO: Implement
-
-    }
-
-    /**
-     * @param kilometers - kilometers you would like to convert to degrees
-     * @return corresponding amount of degrees (Longitude)
-     * */
-    private double kilometersToDegreesLongitude(double kilometers){
-        return 1/(1.11320*Math.cos(Math.toRadians(kilometers)));
-    }
-
-    /**
-     * @param kilometers - kilometers you would like to convert to degrees
-     * @return corresponding amount of degrees (Latitude)
-     * */
-    private double kilometersToDegreesLatitude(double kilometers){
-        return 1/(kilometers*1.10574);
     }
 
     /**
