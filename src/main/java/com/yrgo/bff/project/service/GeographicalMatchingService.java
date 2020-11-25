4edf8a3e05b8b43bf280f208eeeec6a8eb54fb81
Johan Nyberg <johan.e.nyberg@icloud.com>
2020-11-25 12:35:49 +0100

    refactor

--- src/main/java/com/yrgo/bff/project/service/GeographicalMatchingService.java
+++ src/main/java/com/yrgo/bff/project/service/GeographicalMatchingService.java
@@ -17,13 +17,48 @@ public class GeographicalMatchingService {
      * @author Johan Nyberg
     * */
 
-    public boolean match(GpsCoordinates coordinatesPlayerA, double rangePlayerA,
+    public boolean match(GpsCoordinates playerA, GpsCoordinates playerB){
+        final double range = 0.1; // +- 0.1 corresponds to approx 10 km radius in Sweden
+
+        return (isWithinInterval(playerA.getLongitude(),playerB.getLongitude()-range, playerB.getLongitude()+range) &&
+                isWithinInterval(playerA.getLatitude(), playerB.getLatitude()-range, playerB.getLatitude()+range) &&
+                isWithinInterval(playerB.getLatitude(),playerA.getLatitude()-range,playerA.getLatitude()+range) &&
+                isWithinInterval(playerB.getLongitude(),playerA.getLongitude()-range,playerB.getLongitude()+range));
+
+    }
+
+    public boolean matchApprox(GpsCoordinates coordinatesPlayerA, double rangePlayerA,
                          GpsCoordinates coordinatesPlayerB, double rangePlayerB){
 
         double  aLat, aMinLat,aMaxLat, aLong, aMinLong, aMaxLong,
                 bLat, bMinLat, bMaxLat, bLong, bMinLong, bMaxLong;
 
+        //convert from km to degrees
+
         aLat = coordinatesPlayerA.getLatitude();
+        aMinLat = aLat-kilometersToDegreesLatitude(rangePlayerA);
+        aMaxLat = aLat+kilometersToDegreesLatitude(rangePlayerA);
+        aLong = coordinatesPlayerA.getLongitude();
+        aMinLong = aLong-kilometersToDegreesLongitude(rangePlayerA);
+        aMaxLong = aLong+kilometersToDegreesLongitude(rangePlayerA);
+
+        bLat = coordinatesPlayerB.getLatitude();
+        bMinLat = bLat-kilometersToDegreesLatitude(rangePlayerB);
+        bMaxLat = bLat+kilometersToDegreesLatitude(rangePlayerB);
+        bLong = coordinatesPlayerB.getLongitude();
+        bMinLong = bLong-kilometersToDegreesLongitude(rangePlayerB);
+        bMaxLong = bLong+kilometersToDegreesLongitude(rangePlayerB);
+
+        if (isWithinInterval(aLat,bMinLat,bMaxLat) &&
+                isWithinInterval(aLong,bMinLong,bMaxLong) &&
+                isWithinInterval(bLat,aMinLat,aMaxLat) &&
+                isWithinInterval(bLong,aMinLong,aMaxLong))
+        {
+            return true;
+        } else {
+            return false;
+        }
+
 
         //TODO: Implement
 
