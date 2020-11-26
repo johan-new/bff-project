cbed17b5875accf7a5bda5291328d07b1588e380
Gitmannen1337 <56875916+Gitmannen1337@users.noreply.github.com>
2020-11-26 22:30:58 +0100

    Added javadoc comments

--- src/main/java/com/yrgo/bff/project/service/UserAccountServiceImplementation.java
+++ src/main/java/com/yrgo/bff/project/service/UserAccountServiceImplementation.java
@@ -15,13 +15,29 @@ public class UserAccountServiceImplementation implements UserAccountService {
     @Autowired
     UserAccountDataAccess userAccountDataAccess;
 
+    /**
+     * Creates a user and persists it in the database
+     *
+     * @param username - String username of the user you want to create
+     * @param password - String password of the user you want to create
+     * @return An instance of User
+     *
+     * */
     @Override
     public User createUser(String username, String password) {
-        User user = new User(username,password);
+        User user = new User(username, password);
         userAccountDataAccess.save(user);
         return user;
     }
 
+    /**
+     * Removes a user from the database
+     *
+     * @param username - String username of the user you want to remove
+     * @param password - String password of the user you want to remove
+     * @return An instance of User that was deleted
+     *
+     * */
     @Override
     public User removeUser(String username, String password) {
         User user = userAccountDataAccess.findByUserName(username);
@@ -29,6 +45,15 @@ public class UserAccountServiceImplementation implements UserAccountService {
         return user;
     }
 
+    /**
+     * Updates a user with a new password and persists it in the database
+     *
+     * @param username - String of the user you want to update
+     * @param password - String of the user you want to update
+     * @param newPassword - String of the new password
+     * @return An instance of User that was updated
+     *
+     * */
     @Override
     public User updateUser(String username, String password, String newPassword) {
         User user = userAccountDataAccess.findByUserName(username);
@@ -37,11 +62,25 @@ public class UserAccountServiceImplementation implements UserAccountService {
         return user;
     }
 
+    /**
+     * Searches for a user in the database
+     *
+     * @param username - String username of the user you want to find
+     * @param password - String password of the user you want to find
+     * @return An instance of User that was found
+     *
+     * */
     @Override
     public User readUser(String username, String password) {
         return userAccountDataAccess.findByUserName(username);
     }
 
+    /**
+     * Fetches all users from database
+     *
+     * @return a Set of User
+     *
+     * */
     @Override
     public Set<User> findAll() {
         Set<User> users = new HashSet<User>();
