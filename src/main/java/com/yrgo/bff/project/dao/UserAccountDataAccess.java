5c374a72fa64a9adf34439ed23565688f41f7848
Johan Nyberg <johan.e.nyberg@icloud.com>
2020-11-26 10:32:26 +0100

    added comment

--- src/main/java/com/yrgo/bff/project/dao/UserAccountDataAccess.java
+++ src/main/java/com/yrgo/bff/project/dao/UserAccountDataAccess.java
@@ -4,5 +4,13 @@ import com.yrgo.bff.project.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserAccountDataAccess extends CrudRepository<User, String> {
+    /**
+     * findByUserName
+     *
+     * Automagicly configured dao-method
+     *
+     * @param userName - Username of the user you want to find
+     * @return a user, if found, otherwise null
+     * */
     User findByUserName(String userName);
 }
