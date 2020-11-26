30245f34664ceb06d2f02540f95376bf0b933369
Gitmannen1337 <56875916+Gitmannen1337@users.noreply.github.com>
2020-11-26 20:11:04 +0100

    Improved and cleaned up tests

--- src/test/java/com/yrgo/bff/project/UserAccountServiceImplementationTest.java
+++ src/test/java/com/yrgo/bff/project/UserAccountServiceImplementationTest.java
@@ -1,97 +1,76 @@
 package com.yrgo.bff.project;
 
+import com.yrgo.bff.project.dao.UserAccountDataAccess;
 import com.yrgo.bff.project.domain.User;
 import com.yrgo.bff.project.service.UserAccountServiceImplementation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//import static org.junit.jupiter.api.Assertions.assertThat;
 @SpringBootTest
 public class UserAccountServiceImplementationTest {
 
-//    @MockBean
-//    private UserAccountDataAccess userAccountDataAccess;
+    @MockBean
+    private UserAccountDataAccess userAccountDataAccess;
 
     @Autowired
     private UserAccountServiceImplementation userAccountServiceImplementation;
 
-    private User ereko;
-    private User simono;
-    private User juanito;
+    // Variable name is the users name and password as camel notation.
+    private User erekoPassword;
+    private Set<User> userSet;
 
     @BeforeEach
     void init() {
-        ereko = new User("Ereko", "password");
-        simono = new User("Simono", "pw");
-        juanito = new User("Juanito", "admin");
-    }
 
+        erekoPassword = new User("Ereko", "password");
+        Mockito.when(userAccountDataAccess.findByUserName(erekoPassword.getUserName())).thenReturn(erekoPassword);
+        Mockito.when(userAccountDataAccess.save(erekoPassword)).thenReturn(erekoPassword);
 
+        //For testMockFindAll
+        Set<User> users = new HashSet<>();
+        users.add(erekoPassword);
+        Mockito.when(userAccountDataAccess.findAll()).thenReturn(users);
 
-    @Test
-    public void testReadUser() {
-        User userino = userAccountServiceImplementation.createUser("Erik", "Manfredovich");
-        User readUser = userAccountServiceImplementation.readUser(userino.getUserName(), userino.getPassword());
-        assertNotNull(readUser);
-    }
-
-    @Test
-    public void testFindAll() {
-        Set<User> foundUsers = new HashSet<User>();
-        userAccountServiceImplementation.createUser(ereko.getUserName(), ereko.getPassword());
-        userAccountServiceImplementation.createUser(simono.getUserName(), simono.getPassword());
-        userAccountServiceImplementation.createUser(juanito.getUserName(), juanito.getPassword());
-        Set<User> createdUsers = new HashSet<User>();
-        createdUsers.add(ereko);
-        createdUsers.add(simono);
-        createdUsers.add(juanito);
-        foundUsers = userAccountServiceImplementation.findAll();
-        assertEquals(createdUsers, foundUsers);
     }
 
     @Test
-    public void testCreateUser() {
-        User created = userAccountServiceImplementation.createUser("Ereko", "password");
+    void testMockReadUser() {
         User found = userAccountServiceImplementation.readUser("Ereko", "password");
-        assertEquals(created, found);
-    }
-
-
-    @Test
-    public void testRemoveUser() {
-        User heatoN = new User("HeatoN", "1337");
-        userAccountServiceImplementation.createUser(heatoN.getUserName(), heatoN.getPassword());
-        userAccountServiceImplementation.removeUser(heatoN.getUserName(), heatoN.getPassword());
-        User nullUser = userAccountServiceImplementation.readUser(heatoN.getUserName(), heatoN.getPassword());
-        assertNull(nullUser);
+        assertEquals(found, erekoPassword);
     }
 
     @Test
-    public void testUpdateUser() {
-        User newUser = new User("Username", "password");
-        String newPassword = "newPassword";
-        userAccountServiceImplementation.createUser(newUser.getUserName(), newUser.getPassword());
-        User newReadUser = userAccountServiceImplementation.updateUser(newUser.getUserName(), newUser.getPassword(), newPassword);
-        User newPasswordUser = userAccountServiceImplementation.readUser(newUser.getUserName(), newPassword);
-        assertEquals(newPasswordUser.getPassword(), newPassword);
+    void testMockFindAll() {
+        userSet = new HashSet<>();
+        userSet = userAccountServiceImplementation.findAll();
+        assertEquals(userSet.size(), 1);
     }
 
+    @Test
+    public void testMockCreateUser() {
+        User createdUser = userAccountServiceImplementation.createUser(erekoPassword.getUserName(), erekoPassword.getPassword());
+        assertEquals(createdUser, erekoPassword);
+    }
+
+    @Test
+    public void testMockRemoveUser() {
+        userAccountServiceImplementation.removeUser(erekoPassword.getUserName(), erekoPassword.getPassword());
+        Mockito.verify(userAccountDataAccess, Mockito.times(1)).delete(erekoPassword);
+    }
+
+    @Test
+    public void testMockUpdateUser() {
+        String newPassword = "newCoolPassword";
+        userAccountServiceImplementation.updateUser(erekoPassword.getUserName(), erekoPassword.getPassword(), newPassword);
+        assertEquals(erekoPassword.getPassword(), newPassword);
+    }
 }
-
-//    @MockBean
-//    private EmployeeRepository employeeRepository;
-//
-//@Before
-//public void setUp() {
-//        Employee alex = new Employee("alex");
-//
-//        Mockito.when(employeeRepository.findByName(alex.getName()))
-//        .thenReturn(alex);
-//        }
