4ecbea45b695708f02cac3e51c8373467b4dbdcd
Johan Nyberg <johan.e.nyberg@icloud.com>
2020-11-26 10:28:50 +0100

    Done

--- src/main/java/com/yrgo/bff/project/dao/UserAccountDataAccess.java
+++ src/main/java/com/yrgo/bff/project/dao/UserAccountDataAccess.java
@@ -2,11 +2,7 @@ package com.yrgo.bff.project.dao;
 
 import com.yrgo.bff.project.domain.User;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-//@Repository
 public interface UserAccountDataAccess extends CrudRepository<User, String> {
     User findByUserName(String userName);
 }
---
+++ src/test/java/com/yrgo/bff/project/UserAccountDataAccessTest.java
@@ -0,0 +1,31 @@
+package com.yrgo.bff.project;
+
+
+import com.yrgo.bff.project.dao.UserAccountDataAccess;
+import com.yrgo.bff.project.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest
+public class UserAccountDataAccessTest {
+
+    @Autowired
+    UserAccountDataAccess userAccountDataAccess;
+
+    @Test
+    void testFindByUserName(){
+        final String username = "test@mail.com";
+        User user = new User(username,"password");
+        userAccountDataAccess.save(user);
+
+        assertNotNull(userAccountDataAccess.findByUserName(username));
+        assertNull(userAccountDataAccess.findByUserName(username+"asdf"));
+
+    }
+
+
+}
