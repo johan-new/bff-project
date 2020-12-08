61025aee2662db6818133256cc0881e810bb1112
Johan Nyberg <johan.e.nyberg@icloud.com>
2020-12-08 16:13:08 +0100

    dummy data generation

--- src/main/java/com/yrgo/bff/project/Project2Application.java
+++ src/main/java/com/yrgo/bff/project/Project2Application.java
@@ -1,9 +1,22 @@
 package com.yrgo.bff.project;
 
+import com.yrgo.bff.project.domain.ApplicationUser;
+import com.yrgo.bff.project.service.GameService;
+import com.yrgo.bff.project.service.MatchingService;
+import com.yrgo.bff.project.service.NotificationService;
+import com.yrgo.bff.project.service.UserAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class Project2Application {
@@ -13,11 +26,53 @@ public class Project2Application {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Autowired
+	UserAccountService userAccountService;
+
+	@Autowired
+	GameService gameService;
+
+	@Autowired
+	MatchingService matchingService;
+
+
 	public static void main(String[] args) {
-
 		SpringApplication.run(Project2Application.class, args);
+	}
 
 
+	@Component
+	public class AppStartupRunner implements ApplicationRunner {
+
+		@Override
+		public void run(ApplicationArguments args) throws Exception {
+			final String user = "Erik";
+			final String user2 = "Simon";
+			final String user3 = "Johan";
+			final String user4 = "Greven";
+			final String password = "a";
+			final String venue = "Mölndal";
+
+			userAccountService.createUser(user,password);
+			userAccountService.createUser(user2,password);
+			userAccountService.createUser(user3,password);
+			userAccountService.createUser(user4,password);
+
+			Set<ApplicationUser> users = new HashSet<>();
+
+			users.add(userAccountService.readUser(user));
+			users.add(userAccountService.readUser(user2));
+			users.add(userAccountService.readUser(user3));
+
+			gameService.createGame(new Date(),"Göteborg",users);
+
+			/*matchingService.addUserMatchRequest(userAccountService.readUser(user),venue);
+			matchingService.addUserMatchRequest(userAccountService.readUser(user2),venue);
+			matchingService.addUserMatchRequest(userAccountService.readUser(user3),venue);
+			matchingService.addUserMatchRequest(userAccountService.readUser(user4),venue);*/
+
+
+		}
 
 	}
 
