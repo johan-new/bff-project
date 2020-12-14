726e33993bea1fd3454c6e132cae6d94cade3163
Gitmannen1337 <56875916+Gitmannen1337@users.noreply.github.com>
2020-12-14 11:02:52 +0100

    Removed GPS related things

--- src/main/java/com/yrgo/bff/project/service/MatchMakingServiceImplementation.java
+++ src/main/java/com/yrgo/bff/project/service/MatchMakingServiceImplementation.java
@@ -1,6 +1,5 @@
 package com.yrgo.bff.project.service;
 
-import com.yrgo.bff.project.domain.GpsCoordinates;
 import com.yrgo.bff.project.domain.UserAccount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -84,24 +83,6 @@ public class MatchMakingServiceImplementation implements MatchMakingService {
         }
         return locationAndUsers;
     }
-
-    public static boolean match(GpsCoordinates playerA, GpsCoordinates playerB) {
-        final double range = 1; // +- 0.1 corresponds to approx 10 km radius in Sweden
-
-        return (isWithinInterval(playerA.getLongitude(), playerB.getLongitude() - range, playerB.getLongitude() + range) &&
-                isWithinInterval(playerA.getLatitude(), playerB.getLatitude() - range, playerB.getLatitude() + range) &&
-                isWithinInterval(playerB.getLatitude(), playerA.getLatitude() - range, playerA.getLatitude() + range) &&
-                isWithinInterval(playerB.getLongitude(), playerA.getLongitude() - range, playerA.getLongitude() + range));
-
-    }
-
-    /**
-     * Check if a value i within an interval
-     */
-    public static boolean isWithinInterval(double valueToCheck, double minvalue, double maxvalue) {
-        return valueToCheck >= minvalue && valueToCheck <= maxvalue;
-    }
-
 
     public Map<UserAccount, String> getUsersLookingToBeMatched() {
         return usersLookingToBeMatched;
--- src/test/java/com/yrgo/bff/project/service/MatchMakingServiceTest.java
+++ src/test/java/com/yrgo/bff/project/service/MatchMakingServiceTest.java
@@ -1,6 +1,5 @@
 package com.yrgo.bff.project.service;
 
-import com.yrgo.bff.project.domain.GpsCoordinates;
 import com.yrgo.bff.project.domain.UserAccount;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -85,35 +84,5 @@ public class MatchMakingServiceTest {
         assertEquals(usersLookingToBeMatched.size(), 0);
     }
 
-
-    @Test
-    void testIntervalCheck() {
-        assertTrue(MatchMakingServiceImplementation.isWithinInterval(0.9, 0.8, 1.0));
-        assertFalse(MatchMakingServiceImplementation.isWithinInterval(8, 9, 10));
-    }
-
-    @Test
-    void testMatchingService() {
-        //same spot, Gothenburg
-        assertTrue(MatchMakingServiceImplementation.match(new GpsCoordinates(57.708870, 11.974560), new GpsCoordinates(57.708870, 11.974560)));
-
-        //Gothenburg vs Landvetter
-        assertTrue(MatchMakingServiceImplementation.match(new GpsCoordinates(57.708870, 11.974560), new GpsCoordinates(57.395015, 12.165695)));
-
-        //Gothenburg vs Boras
-        assertTrue(MatchMakingServiceImplementation.match(new GpsCoordinates(57.708870, 11.974560), new GpsCoordinates(57.72101, 12.9401)));
-
-        //Gothenburg vs Linköping
-        assertFalse(MatchMakingServiceImplementation.match(new GpsCoordinates(57.708870, 11.974560), new GpsCoordinates(58.41086, 15.62157)));
-
-
-        //edge cases
-        assertFalse(MatchMakingServiceImplementation.match(new GpsCoordinates(600, 60), new GpsCoordinates(60, 60)));
-        assertFalse(MatchMakingServiceImplementation.match(new GpsCoordinates(60, 600), new GpsCoordinates(60, 60)));
-        assertFalse(MatchMakingServiceImplementation.match(new GpsCoordinates(60, 60), new GpsCoordinates(600, 60)));
-        assertFalse(MatchMakingServiceImplementation.match(new GpsCoordinates(60, 60), new GpsCoordinates(60, 600)));
-
-
-    }
 
 }
