d2e172fa077c1b8fccdb542415ed4b61689ffe19
Simon Södergren <simon.sodergren@live.se>
2021-01-15 15:29:11 +0100

    Optimized imports

--- src/test/java/com/yrgo/bff/project/exceptions/BadRequestExceptionsTest.java
+++ src/test/java/com/yrgo/bff/project/exceptions/BadRequestExceptionsTest.java
@@ -1,16 +1,13 @@
 package com.yrgo.bff.project.exceptions;
 
-import com.yrgo.bff.project.domain.UserAccount;
 import com.yrgo.bff.project.exception.BadRequestException;
 import com.yrgo.bff.project.service.FriendsUserAccountServiceImplementationTest;
 import com.yrgo.bff.project.service.UserAccountService;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.json.simple.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @SpringBootTest
