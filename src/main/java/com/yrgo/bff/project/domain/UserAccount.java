b0d1c7d2baadd2684e37e26a844da72bd7c34c9d
Simon Södergren <simon.sodergren@live.se>
2021-01-18 14:14:12 +0100

    Solved conflict and som other stuff

--- src/main/java/com/yrgo/bff/project/domain/UserAccount.java
+++ src/main/java/com/yrgo/bff/project/domain/UserAccount.java
@@ -1,7 +1,6 @@
 package com.yrgo.bff.project.domain;
 
 import com.yrgo.bff.project.exception.BadRequestException;
-import com.yrgo.bff.project.service.UserAccountServiceImplementation;
 import com.yrgo.bff.project.service.useraccount.UserAccountServiceImplementation;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
