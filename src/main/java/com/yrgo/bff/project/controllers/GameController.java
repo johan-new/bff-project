2ad8571365b5bb7ff12397c047733353f105079d
johan-new <johan.e.nyberg@icloud.com>
2021-01-22 11:24:11 +0100

    new comments

--- src/main/java/com/yrgo/bff/project/controllers/GameController.java
+++ src/main/java/com/yrgo/bff/project/controllers/GameController.java
@@ -22,6 +22,9 @@ import java.util.Set;
  * REST Controller for handling games. Not used since the current flow consists of a matching request that
  * gets converted into a Game when the adequate number of participants has joined and been confirmed (by the organizer).
  *
+ * Currently this controller is not implemented by the front end since the core logic and flow
+ * for the users has changed.
+ *
  **/
 
 
@@ -37,34 +40,50 @@ public class GameController {
     private Log log = LogFactory.getLog(getClass());
 
 
-
-    // TODO: for a future admin portal, or remove? A user get the games from ApplicationUserController
+    /**
+     * @param gameId unique game id
+     * @return the requested games data as manually mapped JSON
+     */
     @GetMapping(value = "/game/{gameId}")
     JSONObject readGame(@PathVariable("gameId") String gameId){
         return gameService.readGame(Long.parseLong(gameId)).toJSON();
     }
 
+    /**
+     * @param gameId unique game id
+     * @param newPlayers new players in the current game
+     * @return status code ACCEPTED
+     */
     @PutMapping(value = "/game/{gameId}")
     ResponseEntity updateGame(@PathVariable("gameId") String gameId, @RequestParam("newPlayers") String[] newPlayers) {
-        //TODO: Ability to change date?
         Game game = gameService.readGame(Long.parseLong(gameId));
         gameService.removeGame(Long.parseLong(gameId));
         gameService.createGame(game.getDate(),game.getTime(),game.getVenue(),game.getLocation(), stringArrayToSet(newPlayers));
         return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
 
+    /**
+     * @param gameId unique game id
+     * @return status code NO CONTENT
+     */
     @DeleteMapping(value = "/game/{gameId}")
     ResponseEntity deleteGame(@PathVariable("gameId") String gameId){
         gameService.removeGame(Long.parseLong(gameId));
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    /**
+     * @param players usernames of the players
+     * @return set of UserAccount entities of these players
+     * @throws BadRequestException when incorrect interval
+     */
     private Set<UserAccount> stringArrayToSet(String[] players) throws BadRequestException {
 
         //allowing only 1-3 players in addition to the logged in user
         if (players.length <= 0 || players.length > 3) {
-            log.error("********** " + players);
-            throw new BadRequestException("Too many players");
+            final String msg = "Too few/many players (" + players.length + ")";
+            log.error(msg);
+            throw new BadRequestException(msg);
         }
 
         Set<UserAccount> playersSet = new HashSet<>();
